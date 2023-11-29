9d4793944d3babeb1c2737a771f9610802d51d66
niko <80647004+nemson-source@users.noreply.github.com>
2023-11-29 15:36:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,18 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Set up the file upload storage
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, path.join(__dirname, 'uploads'));
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname); // Keep the original file name
+    cb(null, file.originalname);
   },
 });
 
-// Define a file filter for image and PDF files
 const fileFilter = (req, file, cb) => {
   const allowedFileTypes = ['.jpg', '.jpeg', '.png', '.pdf'];
   const fileExtension = path.extname(file.originalname).toLowerCase();
-
   if (allowedFileTypes.includes(fileExtension)) {
     cb(null, true);
   } else {
@@ -31,29 +28,19 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({ storage, fileFilter });
 
-// Middleware to log connection information
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] Connection from ${req.ip}`);
   next();
 });
-
-// Serve static files from the 'uploads' directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-// Serve the HTML form for file upload
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-// Handle file upload
 app.post('/upload', upload.single('file'), (req, res) => {
   if (req.file) {
     const filePath = path.join('/uploads', req.file.filename);
     console.log(`[${new Date().toISOString()}] File upload started - Type: ${req.file.mimetype}, Name: ${req.file.originalname}`);
-
-    // Determine whether the file is an image or a PDF
     if (req.file.mimetype.startsWith('image')) {
-      // If it's an image, display it using an <img> tag
       console.log(`[${new Date().toISOString()}] Image file uploaded successfully`);
       res.send(`
         <h2>File uploaded successfully!</h2>
@@ -62,7 +49,6 @@ app.post('/upload', upload.single('file'), (req, res) => {
         <img src="${filePath}" alt="Uploaded Image">
       `);
     } else if (req.file.mimetype === 'application/pdf') {
-      // If it's a PDF, embed it using an <iframe> tag
       console.log(`[${new Date().toISOString()}] PDF file uploaded successfully`);
       res.send(`
         <h2>File uploaded successfully!</h2>
@@ -71,7 +57,6 @@ app.post('/upload', upload.single('file'), (req, res) => {
         <iframe src="${filePath}" style="width: 100%; height: 500px;" frameborder="0"></iframe>
       `);
     } else {
-      // For unsupported file types
       console.log(`[${new Date().toISOString()}] Unsupported file type. Unable to display.`);
       res.send(`
         <h2>File uploaded successfully!</h2>
