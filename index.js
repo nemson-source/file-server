1bae1b5522a4e65c0a69951120cfb165533dde34
niko <80647004+nemson-source@users.noreply.github.com>
2024-02-22 20:39:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
-const fs = require('fs')
+const fs = require('fs');
+const ejs = require('ejs');
 
 function createFolder(folderName) {
   if (!fs.existsSync(folderName)) {
@@ -11,11 +12,15 @@ function createFolder(folderName) {
     console.log(`Folder "${folderName}" already exists.`);
   }
 }
+
 const folderName = 'uploads';
 createFolder(folderName);
 
 const app = express();
 const port = 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -23,32 +28,23 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
-    console.log(`og: ${file.originalname}`)
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  const allowedFileTypes = ['.jpg', '.jpeg', '.png', '.pdf'];
-  const fileExtension = path.extname(file.originalname).toLowerCase();
-  if (allowedFileTypes.includes(fileExtension)) {
-    cb(null, true);
-  } else {
-    const errorMessage = 'Type de fichier invalide. Seules les images (jpg, jpeg, png) et PDF sont autorisées.';
-    console.error(`[${new Date().toISOString()}] ${errorMessage}`);
-    cb(new Error(errorMessage));
-  }
-};
-
-const upload = multer({ storage, fileFilter });
+const upload = multer({ storage });
 
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] Connection from ${req.ip}`);
   next();
 });
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  const files = fs.readdirSync(path.join(__dirname, 'uploads'));
+  res.render('index', { files });
 });
+
 app.post('/upload', upload.single('file'), (req, res) => {
   if (req.file) {
     const filePath = path.join('/uploads', req.file.filename);
@@ -56,31 +52,31 @@ app.post('/upload', upload.single('file'), (req, res) => {
     if (req.file.mimetype.startsWith('image')) {
       console.log(`[${new Date().toISOString()}] Image file uploaded successfully`);
       res.send(`
-        <h2>Fichier téléchargé avec succès!</h2>
-        <p>Type de Fichier: ${req.file.mimetype}</p>
-        <p>Nom de Fichier: ${req.file.originalname}</p>
+        <h2>File uploaded successfully!</h2>
+        <p>File Type: ${req.file.mimetype}</p>
+        <p>File Name: ${req.file.originalname}</p>
         <img src="${filePath}" alt="Uploaded Image">
       `);
     } else if (req.file.mimetype === 'application/pdf') {
       console.log(`[${new Date().toISOString()}] PDF file uploaded successfully`);
       res.send(`
-        <h2>Fichier téléchargé avec succès!</h2>
-        <p>Type de Fichier: ${req.file.mimetype}</p>
-        <p>Nom de Fichier: ${req.file.originalname}</p>
+        <h2>File uploaded successfully!</h2>
+        <p>File Type: ${req.file.mimetype}</p>
+        <p>File Name: ${req.file.originalname}</p>
         <iframe src="${filePath}" style="width: 100%; height: 847px;" frameborder="0"></iframe>
       `);
     } else {
-      console.log(`[${new Date().toISOString()}] Type de fichier non pris en charge. Impossible d'afficher.`);
+      console.log(`[${new Date().toISOString()}] Unsupported file type. Unable to display.`);
       res.send(`
-        <h2>Fichier téléchargé avec succès!</h2>
-        <p>Type de Fichier: ${req.file.mimetype}</p>
-        <p>Nom de Fichier: ${req.file.originalname}</p>
-        <p>Type de fichier non pris en charge. Impossible d'afficher.</p>
+        <h2>File uploaded successfully!</h2>
+        <p>File Type: ${req.file.mimetype}</p>
+        <p>File Name: ${req.file.originalname}</p>
+        <p>Unsupported file type. Unable to display.</p>
       `);
     }
   } else {
     console.log(`[${new Date().toISOString()}] File upload failed`);
-    res.status(400).send('Échec du téléchargement du fichier.');
+    res.status(400).send('File upload failed.');
   }
 });
 
